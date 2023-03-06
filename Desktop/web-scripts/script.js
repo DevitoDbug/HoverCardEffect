5dfaa4b174033b5ba2fc67845cb0fbd60db43486
DevitoDbug <Davidochiengy@gmail.com>
2023-03-06 06:29:45 +0300

    Added css and JavaScript

--- Desktop/web-scripts/script.js
+++ Desktop/web-scripts/script.js
@@ -1,1 +1,18 @@
-document.write("Hellow Js");
+const subtitle = document.getElementsByClassName("card-subtitle")[0];
+const createWord =(text, index) => {
+    const word  = document.create("span");
+
+    word.innerHTML - `${text}`;
+
+    word.classList.add("card-subtitle-word");
+
+    word.style.transitionDelay = `${index * 40} ms`;
+
+    return word;
+}
+
+const addWord = (text, index) => subtitle.appendChild(createWord(text,index));
+
+const createSubtitle = text => text.split(" ").map(addWord);
+
+createSubtitle("This is a random subtitle");
